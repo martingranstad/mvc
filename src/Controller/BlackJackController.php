645bb5497d5bac96ea779986ddcaf4c7fbd71209
Martin Granstad <martin.granstad@gmail.com>
2024-04-18 13:59:14 +0200

    Create landning page for game

---
+++ src/Controller/BlackJackController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlackJackController extends AbstractController
+{
+    #[Route("/game", name: "game")]
+    public function game(): Response
+    {
+        return $this->render('game.html.twig');
+    }
+
+    #[Route("/black-jack", name: "black-jack")]
+    public function blackJack(): Response
+    {
+        return $this->render('game.html.twig');
+    }
+}
