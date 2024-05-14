0e20568d1a34bc6f766eb5e3e02b57b1d2ba49e0
Martin Granstad <martin.granstad@gmail.com>
2024-05-14 17:50:54 +0200

    Adds functional test for a user that does not exist

---
+++ tests/Project/ProjectLoginTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProjectLoginTest extends WebTestCase
+{
+    public function testUserLoginUserDoesNotExist(): void
+    {
+        $client = static::createClient();
+
+        // Johan is not a user, he visits the /proj route
+        $client->request('GET', '/proj');
+
+        // Johan succesfully arrives at the /proj route
+        $this->assertResponseIsSuccessful();
+
+        // There he finds a form with a textfield with the label "Ditt namn:" and a submit button
+        $this->assertSelectorExists('form');
+        $this->assertSelectorTextContains('label', 'Ditt namn:');
+        $this->assertSelectorExists('input[type="text"]');
+        $this->assertSelectorExists('input[type="submit"]');
+
+        // Johan fills in the textfield with his name and submits the form
+        $client->submitForm('Skicka', [
+            'name' => 'Johan'
+        ]);
+
+        // Johan is redirected to the /proj/game route
+        $this->assertResponseRedirects('/proj/game');
+
+        // Johan faces a welcome message greeting him and informing him that he's account balance is 1000
+        $this->assertSelectorTextContains('h1', 'Välkommen Johan!');
+        $this->assertSelectorTextContains('p', 'Ditt konto innehåller 1000 kr');
+
+    }
+}
+        
