876b507b7a480e82ef84d47b4e379198f4f56363
Martin Granstad <martin.granstad@gmail.com>
2024-05-28 12:48:39 +0200

    Fixing scrutinizer errors

---
+++ public/build/project.3d8a0760.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[354],{514:()=>{console.log("This log comes from assets/project.js - welcome to AssetMapper! 🎉")}},s=>{var e;e=514,s(s.s=e)}]);
